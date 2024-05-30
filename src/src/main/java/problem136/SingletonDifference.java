69e0ef0b0c4da07633604f38f022ad0ebaaceada
Nobilis <nikolaev-evgeny@yandex.ru>
2024-05-30 11:14:36 +0300

    2024-05-30/11.13 Удаление лишних файлов в пакете problem136.

--- src/src/main/java/problem136/SingletonDifference.java
+++ src/src/main/java/problem136/SingletonDifference.java
@@ -1,48 +1,57 @@
 package problem136;
 
 public class SingletonDifference {
+    private static final int NUMBERS_SIZE = 50_000_000;
+    private static final int SQUARES_SIZE = 100_000_000;
 
-//    final private static int SIZE = 50_000_000;
-            final private static int SIZE = 100;
-    final private static int[] numbers = new int[SIZE];
+    private static final int[] numbers = new int[NUMBERS_SIZE];
+    private static long[] squares;
 
     public static void main(String[] args) {
+
         long start = System.currentTimeMillis();
+
+        fillSquares();
         fillNumbers();
-        int count = countSingletons();
+        int count = countSingletonDifference();
+
         long end = System.currentTimeMillis();
+
         System.out.println(count);
         System.out.println(end - start);
+
     }
 
     private static void fillNumbers() {
-        long start = System.currentTimeMillis();
-        for (int i = 1; i < 10_000_000; i++) {
-            for (int j = 1; j < 1_000_000; j++) {
-                long res = (long) Math.pow(i + 2 * j, 2)
-                        - (long) Math.pow(i + j, 2)
-                        - (long) Math.pow(i, 2);
-                if (res >= SIZE) {
-                    if (i % 100_000 == 0) {
-                        long now = System.currentTimeMillis();
-                        System.out.println(i
-                                + " : "
-                                + j
-                                + " "
-                                + ((now - start) / 1000));
-                    }
+        final int limit = squares.length - 2;
+        int minStep = 1;
+        for(int ind = 1; ind < limit; ind++) {
+            for(int step = minStep; ind + step * 2 < squares.length; step++) {
+                long res = squares[ind + step * 2] - squares[ind + step] - squares[ind];
+                if(res < 0) {
+                    minStep++;
+                }
+                else if(res >= numbers.length) {
                     break;
                 }
-                if (res > 0) {
-                    numbers[(int) res]++;
+                else {
+                    numbers[(int)res]++;
                 }
             }
         }
     }
 
-    private static int countSingletons() {
+    private static void fillSquares() {
+        squares = new long[SQUARES_SIZE];
+        squares[1] = 1;
+        for (int i = 2; i < squares.length; i++) {
+            squares[i] = (long) i * i;
+        }
+    }
+
+    private static int countSingletonDifference() {
         int count = 0;
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] == 1) {
                 count++;
             }
