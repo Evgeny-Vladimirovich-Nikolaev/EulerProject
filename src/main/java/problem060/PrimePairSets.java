5f5ad970b70ea5a12f6dd5c9e2b7d454ee3425d7
Nobilis <nikolaev-evgeny@yandex.ru>
2024-06-11 01:53:18 +0400

    2024-06-11/00.52 Задача 60 в процессе решения.

--- src/main/java/problem060/PrimePairSets.java
+++ src/main/java/problem060/PrimePairSets.java
@@ -31,12 +31,32 @@ public class PrimePairSets {
     private static void findMinPrimePairSetsSum() {
         for(int i = 3; i < max; i++) {
             if(compositeNumbers[i]) continue;
-            for(int j = i + 2; j < max; j++) {
-                if(compositeNumbers[j]) continue;
-                for(int k = j + 2; j < max; j++) {
-                    for(int m = k + 2; m < max; m++) {
-                        for(int n = m + 2; n < max; n++) {
-
+            for(int j = i + 2; i + j < max; j++) {
+                if(compositeNumbers[j]
+                || !checkPair(i, j)) {
+                    continue;
+                }
+                for(int k = j + 2; i + j + k < max; k++) {
+                    if(compositeNumbers[j]
+                            || !checkPair(i, k)
+                            || !checkPair(j, k)) {
+                        continue;
+                    }
+                    for(int m = k + 2; i + j + k + m < max; m++) {
+                        if(compositeNumbers[j]
+                                || !checkPair(i, m)
+                                || !checkPair(j, m)
+                                || !checkPair(k, m)) {
+                            continue;
+                        }
+                        for(int n = m + 2; i + j + k + m + n < max; n++) {
+                            if(compositeNumbers[j]
+                                    || !checkPair(i, m)
+                                    || !checkPair(j, m)
+                                    || !checkPair(k, m)) {
+                                continue;
+                            }
+                            max = i + j + k + m + n;
                         }
                     }
                 }
