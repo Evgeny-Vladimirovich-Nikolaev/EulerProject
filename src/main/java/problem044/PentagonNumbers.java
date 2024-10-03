df7f50342b20111f473b6c855e0db147e51c585c
Nobilis <nikolaev-evgeny@yandex.ru>
2024-10-03 13:29:12 +0300

    2024-10-03/13.28 PentagonNumbers Задача 44 решена.

--- src/main/java/problem044/PentagonNumbers.java
+++ src/main/java/problem044/PentagonNumbers.java
@@ -4,48 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.function.UnaryOperator;
 
-//public class PentagonNumbers {
-//
-//    private static final List<Integer> numbers = new ArrayList<>();
-//    private static final Map<Integer, int[]> pair = new TreeMap<>();
-//    private static int minDiff = 30_000;
-//
-//    public static void main(String[] args) {
-//        long start = System.currentTimeMillis();
-//        fillNumbers(integer -> integer * (3 * integer - 1) / 2);
-//        long end = System.currentTimeMillis();
-//        System.out.println(minDiff);
-//        System.out.println((end - start) / 60_000 + "min " + ((end - start) % 60_000) / 1000 + "sec");
-//
-//    }
-//
-//    private static void fillNumbers(UnaryOperator<Integer> pentagon) {
-//        int n = 1;
-//        numbers.add(pentagon.apply(n++));
-//        numbers.add(pentagon.apply(n++));
-//        while (checkDifference()) {
-//            numbers.add(pentagon.apply(n++));
-//            findNewDiff();
-////            if(pair.size() % 300 == 0) {
-////                System.out.println(pair);
-////            }
-//        }
-//    }
-//
-//    private static boolean checkDifference() {
-//        return numbers.getLast() - numbers.get(numbers.size() - 2) < minDiff;
-//    }
-//
-//    private static void findNewDiff() {
-//        for (int i = numbers.size() - 2; i > 0; i--) {
-//                int diff = numbers.getLast() - numbers.get(i);
-//                if (numbers.contains(diff)) {
-//                    pair.putIfAbsent(diff, new int[]{numbers.get(i), numbers.getLast()});
-//                }
-//
-//        }
-//    }
-//
-//}
+public class PentagonNumbers {
+
+    private static List<Integer> pentagonalNumbers = new ArrayList<>(1000);
+    private static Map<Integer, Integer> differences = new TreeMap<>();
+    public static void main(String[] args) {
+
+        int sum = findPentagonalSum();
+        System.out.println(differences.get(sum));
+        System.out.println(pentagonalNumbers.size());
+    }
+
+    private static int findPentagonalSum() {
+        int next;
+        int currentDiff = 4;
+        pentagonalNumbers.add(1);
+        do {
+            int previous = pentagonalNumbers.get(pentagonalNumbers.size() - 1);
+            next = previous + currentDiff;
+            pentagonalNumbers.add(next);
+            findDifferences();
+            currentDiff += 3;
+        } while(!differences.containsKey(next));
+        return next;
+    }
+
+    private static void findDifferences() {
+        int last = pentagonalNumbers.get(pentagonalNumbers.size() - 1);
+        for(int i = 0; i < pentagonalNumbers.size() - 1; i++) {
+            int diff = last - pentagonalNumbers.get(i);
+            if(pentagonalNumbers.contains(diff)) {
+                differences.put(last + pentagonalNumbers.get(i), diff);
+            }
+        }
+    }
+
+}
